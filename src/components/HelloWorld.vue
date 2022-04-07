a96da2c6fb338383f802fd8445c3afc6b645ae87
Hasmed <EfJim_@hotmail.com>
2022-04-06 19:25:00 -0500

    generalchanges

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,32 +15,32 @@
           <v-card-text > 
                <v-flex >
 
-             <v-img :src="require('../assets/logo_v1.png')" class="my-5"  contain height="200" />
+             <v-img :src="require('../assets/logo_v1.png')" class="my-5"  contain height="150" />
 
                        <v-form>
-                        <v-text-field label="Users" name="Users" type="text" />
+                        <v-text-field label="User" name="Users" type="text" color="#041C2C"/>
                         <v-text-field label="Password" 
                           :type="showPassword ? 'text'  : 'password'" 
                           :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                          color="#041C2C"
                           @click:append="showPassword = !showPassword"/>
                           </v-form>
                           
                           <br>
-                          <v-btn block >LOGIN</v-btn>
+                          <v-btn block color="#041C2C" :elevation="0"  height="50" >LOGIN</v-btn>
                      <br> 
                       <br>
 
-                    <a>Forgot your Password?</a>
+                    <a><p class="font-weight-medium">Forgot your Password?</p></a>
                     
                      <br>
-                     <br>
-                     <h3>Or</h3>
+                     <p  class="second font-weight-medium" color="#041C2C">Or</p>
                      <br>
 
                      
-                      <v-btn block ><img src="../assets/google.png" alt="" contain height="30">sing in with google</v-btn>
+                      <v-btn block :elevation="6"  height="50"><img src="../assets/google.png" alt="" contain height="30"> sing in with google</v-btn>
                         <br>
-                        <v-btn  block ><img src="../assets/apple.png" alt="" contain height="30">apple id</v-btn>
+                        <v-btn  block :elevation="6"  height="50"><img src="../assets/apple.png" alt="" contain height="30"> sing in with apple</v-btn>
                         <br>
                     </v-flex>
                     </v-card-text> 
@@ -77,12 +77,10 @@ export default {
 
 
 <style scoped>
-a{
-    margin:0px 0px 0px 140px;
 
-}
-h3{
+.second{
   margin:0px 0px 0px 205px;
+  color: #041C2C;
 }
 .container{
     margin:  20px auto;
@@ -90,12 +88,27 @@ h3{
 .v-card-text{
   border: 2px solid rgb(82, 0, 14);
 }
+
+.col-md-16 .col-5{
+  height: 770px;
+}
 .col-md-7.col-6{
 background: url(../assets/PDA.jpg);
 width: 700px;
-height: 738px;
+height: 755px;
 background-size: 1150px;
-background-position-x: -100px;
+background-position-x: -120px;
 }
 
+/* ||||||||||||||||||||||||| loginButtonStyles ||||||||||||||||||||||||| */
+.v-btn{
+ box-shadow: 0px 3px 5px;
+}
+
+/* ||||||||||||||||||||||||| pLinkForgotStyles ||||||||||||||||||||||||| */
+p{
+    margin: -20px 0px 0px 135px;
+  color: #041C2C;
+  font-size: 18px;
+}
 </style>
